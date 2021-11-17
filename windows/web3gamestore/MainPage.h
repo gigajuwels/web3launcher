cad53f36e7940263cbc70d3f6a76a9135e9cba09
Julia Seward <giga.juwels@gmail.com>
2021-11-17 17:42:02 -0500

    first commit

---
+++ windows/web3gamestore/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::web3gamestore::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::web3gamestore::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
