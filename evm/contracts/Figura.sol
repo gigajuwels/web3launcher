8a891c250500c9ea46524e7bc64f431857e83252
Julia Seward <giga.juwels@gmail.com>
2021-11-28 14:17:47 -0500

    update contracts to add buying to FIgura token and more GameData

--- evm/contracts/Figura.sol
+++ evm/contracts/Figura.sol
@@ -1,37 +1,94 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
+import './GameDirectory.sol';
+import './AggregatorV3Interface.sol';
 
-contract Figura is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, AccessControlUpgradeable {
+contract Figura is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
     CountersUpgradeable.Counter private _tokenIdCounter;
+    GameDirectory public gameDirectory;
+    AggregatorV3Interface public ethToUsdFeed;
+
+    mapping(uint256 => uint256) public gameBalance;
+    mapping(uint256 => uint256) public tokenIdGameMapping;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
-    function initialize() initializer public {
-        __ERC721_init("Figura", "FIG");
+    function initialize(address _directoryAddress, address _chainlinkFeed) initializer public {
+        __ERC721_init('Figura', 'FIG');
         __ERC721Enumerable_init();
-        __ERC721URIStorage_init();
         __AccessControl_init();
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
+
+        gameDirectory = GameDirectory(_directoryAddress);
+        ethToUsdFeed = AggregatorV3Interface(_chainlinkFeed);
     }
 
-    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+    function buyGame(uint256 gameId, address newOwner) external payable {
+        require(gameDirectory.exists(gameId), 'Game does not exist');
+
+        uint256 etherPayment = msg.value;
+
+        uint256 weiRequired = _convertUsdPriceToWei(gameId);
+
+        require(etherPayment >= weiRequired, 'Did not send enough to meet payment');
+
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _safeMint(newOwner, tokenId);
+        tokenIdGameMapping[tokenId] = gameId;
+
+        gameBalance[gameId] += weiRequired;
+
+        if (etherPayment > weiRequired) {
+            //Refund any extra
+            payable(msg.sender).transfer(etherPayment - weiRequired);
+        }
+        //_setTokenURI(tokenId, uri);
+    }
+
+    function _convertUsdPriceToWei(uint256 gameId) internal view returns (uint256) {
+        int price;
+
+        (
+            , 
+            price,
+            ,
+            ,
+        ) = ethToUsdFeed.latestRoundData();
+
+        uint256 decimals = ethToUsdFeed.decimals();
+        uint256 currentPrice = 0;
+
+        if (decimals == 18) {
+            currentPrice = uint256(price);
+        } else if (decimals < 18) {
+            //Convert price to wei
+            uint256 factor = 10 ** (18 - decimals);
+            uint256 convertedPrice = currentPrice * factor;
+
+            currentPrice = convertedPrice;
+        } else {
+            revert('Feed is using unsupported decimals');
+        }
+
+        uint256 usdPrice;
+        (,,,,,,usdPrice) = gameDirectory.games(gameId);
+
+        uint256 weiRequired = usdPrice / (currentPrice / 1e18);
+
+        return weiRequired;
     }
 
     // The following functions are overrides required by Solidity.
@@ -45,7 +102,7 @@ contract Figura is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable
 
     function _burn(uint256 tokenId)
         internal
-        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        override
     {
         super._burn(tokenId);
     }
@@ -53,10 +110,11 @@ contract Figura is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        override
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        uint256 gameId = tokenIdGameMapping[tokenId];
+        return gameDirectory.tokenURI(gameId);
     }
 
     function supportsInterface(bytes4 interfaceId)
--- evm/contracts/GameDirectory.sol
+++ evm/contracts/GameDirectory.sol
@@ -3,13 +3,13 @@
 #TODO add title, game description, image URL, cost (USD only), and webhook address, publisher name */
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
-import "./Base64.sol";
+import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
+import './Base64.sol';
 
 contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
@@ -18,18 +18,23 @@ contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgr
         string description;
         string publisher;
         string gameCoverURL;
+        string website;
         string webhook;
         uint256 price;
+        address[] gameTokens;
     }
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
     CountersUpgradeable.Counter private _tokenIdCounter;
     mapping (uint256 => GameData) public games;
+
+    event GameCreated(uint256 indexed gameId, address indexed owner);
+    event GameUpdated(uint256 indexed gameId);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function initialize() initializer public {
-        __ERC721_init("Game Directory", "GDN");
+        __ERC721_init('Game Directory', 'GDN');
         __ERC721Enumerable_init();
         __AccessControl_init();
 
@@ -37,11 +42,32 @@ contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgr
         _setupRole(MINTER_ROLE, msg.sender);
     }
 
+    function getGameTokens(uint256 gameId) external view returns (address[] memory) {
+        require(_exists(gameId), 'Game does not exist');
+
+        return games[gameId].gameTokens;
+    }
+
+    function getGameToken(uint256 gameId, uint256 index) external view returns (address) {
+        require(_exists(gameId), 'Game does not exist');
+        require(index < games[gameId].gameTokens.length, 'Index out of bounds');
+
+        return games[gameId].gameTokens[index];
+    }
+
+    function getGameTokenCount(uint256 gameId) external view returns (uint256) {
+        require(_exists(gameId), 'Game does not exist');
+
+        return games[gameId].gameTokens.length;
+    }
+
     function submitGame(address to, GameData memory gamesub) public{
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         games[tokenId]=gamesub;
+
+        emit GameCreated(tokenId, to);
     }
 
     // The following functions are overrides required by Solidity.
@@ -55,7 +81,7 @@ contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgr
 
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
-            return "0";
+            return '0';
         }
         uint j = _i;
         uint len;
@@ -73,6 +99,15 @@ contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgr
             _i /= 10;
         }
         return string(bstr);
+    }
+
+    function updateGame(uint256 tokenId, GameData calldata newData) external {
+        require(_exists(tokenId), 'Game does not exist');
+        require(msg.sender == ownerOf(tokenId), 'Caller must be the current game admin');
+
+        games[tokenId] = newData;
+
+        emit GameUpdated(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
@@ -96,5 +131,9 @@ contract GameDirectory is Initializable, ERC721Upgradeable, ERC721EnumerableUpgr
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
     }
 }
